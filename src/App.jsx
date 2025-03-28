37103f55479d82f1907c664558609e8dfc415a82
Yashyas <Ym130602@gmail.com>
2025-03-28 15:49:11 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import SignupPage from "./components/SignupPage";
 import AccountSettings from "./components/AccountSettings";
 function App() {
   return (
-    <Router>
+  
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/account-settings" element={<AccountSettings />} />
       </Routes>
-    </Router>
+  
   )
 }
 
